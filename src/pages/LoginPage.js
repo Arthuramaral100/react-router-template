ce84a48ea709ddca727f2600a4cd03c42a22ec83
Arthuramaral100 <arthuramaral57@gmail.com>
2022-11-23 11:14:43 -0300

    Trabalhando com react-router

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import Header from "../components/Header"
+function LoginPage(){
+    return(
+        <>
+        <Header />
+        <h1>página de Login</h1>
+        </>
+    )
+}
+
+export default LoginPage
